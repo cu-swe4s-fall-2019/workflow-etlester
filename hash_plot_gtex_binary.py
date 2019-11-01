0703814fff0b128bdad4e809baf445c7f5b3c304
Evan Lester <ETLester1@gmail.com>
2019-10-31 23:51:30 -0600

    replated parallel arrays with two hash tables

--- hash_plot_gtex_binary.py
+++ hash_plot_gtex_binary.py
@@ -1,8 +1,7 @@
 import data_viz as dv
 import sys
 import argparse
-from hash_tables_etlester import hash_tables
-from hash_tables_etlester import hash_functions
+from hash_tables_etlester import hash_tables as ht
 
 
 def initialize():
@@ -84,7 +83,6 @@ def main():
             sample_info_header = l.rstrip().split('\t')
         else:
             samples.append(l.rstrip().split('\t'))
-
 #   find the column that contains the sample type info
     group_col_idx = linear_search(sample_type_col_name, sample_info_header)
     # print(group_col_idx)
@@ -92,13 +90,24 @@ def main():
     sample_id_col_idx = linear_search(sample_id_col_name, sample_info_header)
     # print(sample_id_col_idx)
 
+
+
+    #make hash table for the sample types and sample ids
     groups = []
     members = []
-
+    tissue_types_hash_table = ht.ChainedHash(1000, ht.h_rolling)
+    # print(samples)
     for row_idx in range(len(samples)):
-        sample = samples[row_idx]
-        sample_name = sample[sample_id_col_idx]
-        curr_group = sample[group_col_idx]
+        curr_group = samples[row_idx][group_col_idx]
+        sample_name = samples[row_idx][sample_id_col_idx]
+        key = curr_group
+        value = sample_name
+        search_result = tissue_types_hash_table.search(curr_group)
+        if (search_result is None):
+            tissue_types_hash_table.add(curr_group, [sample_name])
+            groups.append(curr_group)
+        else:
+            search_result.append(sample_name)
 #       searches a growing list named groups for the current group. and returns
 #       the index of the current_group in the list groups
 #       if the current group is not found, it adds the current group to the
@@ -107,14 +116,15 @@ def main():
         if curr_group_idx == -1:
             curr_group_idx = len(groups)
             groups.append(curr_group)
-            members.append([])
 #        if the current group is present in groups, we append the sample name
 #        to the growing members list at the location, current group index
-        members[curr_group_idx].append(sample_name)
+
+
+    #load gene counts so that we can make a hash table with the gene ID as
+    #keys and the gene counts for the specified gene
     version = None
     dim = None
     data_header = None
-
     gene_name_col = 1
 
     group_counts = [[] for i in range(len(groups))]
@@ -128,26 +138,45 @@ def main():
             continue
 
         if data_header is None:
-            data_header = []
-            i = 0
-            for field in l.rstrip().split('\t'):
-                data_header.append([field, i])
-                i += 1
-            data_header.sort(key=lambda tup: tup[0])
+            data_header = l.rstrip().split('\t')
             continue
-
+            # data_header = []
+            # i = 0
+            # for field in l.rstrip().split('\t'):
+            #     data_header.append([field, i])
+            #     i += 1
+            # data_header.sort(key=lambda tup: tup[0])
+            # continue
         A = l.rstrip().split('\t')
-
+        #make hash table of Key = sample_ID and Value = gene_counts
         if A[gene_name_col] == gene_name:
-            for group_idx in range(len(groups)):
-                for member in members[group_idx]:
-                    member_idx = binary_search(member, data_header)
-                    if member_idx != -1:
-                        group_counts[group_idx].append(int(A[member_idx]))
-            break
+            #initiate gene counts hash table
+            gene_counts_hash_table = ht.ChainedHash(1000000, ht.h_rolling)
+            #loop through
+            for i in range(2, len(data_header)):
+                gene_counts_hash_table.add(data_header[i], int(A[i]))
+
+    #search first hash table using groups as keys
+    #this will return sample IDs that can be used as keys for
+    #hash table 2. this will return gene counts which can be plotted
+
+    # print(tissue_types_hash_table.T)
+    group_counts = []
+    for i in range(len(groups)):
+        counts = []
+        sample_id = tissue_types_hash_table.search(groups[i])
+        if sample_id is None:
+            continue
+        for j in sample_id:
+            sample_counts = gene_counts_hash_table.search(j)
+            if sample_counts is None:
+                continue
+            counts.append(sample_counts)
+        group_counts.append(counts)
 
     dv.boxplot(group_counts, groups, gene_name,
                sample_type_col_name, plot_name)
+
 
 
 if __name__ == '__main__':
